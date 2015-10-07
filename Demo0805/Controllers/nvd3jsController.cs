3cd45b637c87695ef42f69a46a60746a944d7e74
Benknightdark <moon791017@gmail.com>
2015-10-07 16:09:48 +0800

    upload20151007

---
+++ Demo0805/Controllers/nvd3jsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo0805.Controllers
+{
+    public class nvd3jsController : Controller
+    {
+        //
+        // GET: /nvd3js/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
